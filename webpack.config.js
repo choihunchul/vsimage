bb4dc5b9928d0500e053d66a3935f68496626e3f
hunchulchoi <nettlica@live.co.kr>
2026-05-31 14:03:38 +0900

    chore: scaffold extension build structure

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/extension.ts',
+  target: 'node',
+  mode: 'development',
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'ts-loader' }]
+      }
+    ]
+  },
+  output: {
+    filename: 'extension.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs'
+  },
+  externals: {
+    vscode: 'commonjs vscode'
+  },
+  devtool: 'source-map'
+};
